c982c82b828d237d3415865833d12831a0928b5c
thermalogic <cmh@seu.edu.cn>
2018-11-24 11:57:24 +0800

    update S2019

--- P5/cpp/fastipow.cpp
+++ P5/cpp/fastipow.cpp
@@ -1,22 +1,39 @@
 /*
 g++ -o ../bin/fastipow fastipow.cpp
+../bin/fastipow
 */
 #include <iostream>
 
 using namespace std;
 
-double ipow(double x, unsigned int n)
-/* The Repeated Squaring Method : calculate x to the n power,
-    n>=0
-    returns 0.0^0 = 1.0, so continuous in x
+/*  
+    ipow:  repeated squaring method
+        https://en.wikipedia.org/wiki/Exponentiation_by_squaring
 */
+double ipow(double x, int n)
 {
     double value = 1.0;
+
+    if (!n)
+        return 1.0; /* x^0 = 1 */
+
+    if (n == 1)
+        return x;
+
+    if (x == 0.0)
+        return 0.0; /* 0^n = 0 */
+
+    if (n < 0)
+    {
+        n = -n;
+        x = 1.0 / x;
+    }
+
     do
     {
-        if (n & 1)
-            value *= x; /* for n odd */
-        n >>= 1;
+        if (n & 1) /* n is odd */
+            value *= x;
+        n >>= 1; /* n/2  */
         x *= x;
     } while (n);
 
@@ -26,10 +43,12 @@ double ipow(double x, unsigned int n)
 int main()
 {
     cout << "\nThe simple example of the repeated squaring method by" << endl;
-    cout << "          Name: "<< "Change the text to your name" << endl;
-    cout << "    Student ID: "<< "Change the text to your student ID" << endl;
-   
+    cout << "          Name: "
+         << "Change the text to your name" << endl;
+    cout << "    Student ID: "
+         << "Change the text to your student ID" << endl;
+
     double x = 2.0;
-    int n=8;  
-    cout << "\n ipow(x, n): "<< x<<"^"<<n<<"="<< ipow(x, n) <<endl;
+    int n = 8;
+    cout << "\n ipow(x, n): " << x << "^" << n << "=" << ipow(x, n) << endl;
 };
