0e44e595353867817ef854ce1b25622a2311a811
thermalogic <cmh@seu.edu.cn>
2019-03-07 20:02:59 +0800

    update P1

--- P1/cpp/hello.cpp
+++ P1/cpp/hello.cpp
@@ -1,11 +1,13 @@
 /*
 g++ -o hello hello.cpp
+
+./hello
 */
 #include <iostream>
 using namespace std;
  
 int main() {
-   cout << "      Name: "<<"Change the text to your name"<<endl;
+   cout << "      Name: "<<"Change the text to your name(PIN YIN)"<<endl;
    cout << "Student ID: "<<"Change the text to your student ID"<<endl;
    return 0;
 }
