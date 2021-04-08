9485e027b2cc69c663a5479d09e3d8a600d06249
thermalogic <cmh@seu.edu.cn>
2021-04-08 20:48:31 +0800

    update P3

--- P3/demo_p3/include/sort.h
+++ P3/demo_p3/include/sort.h
@@ -4,11 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void insertionSort(int a[], int size);
-void selectionSort(int a[], int size);
-void mergeSort(int a[], int size);
+
 void bubbleSort(int a[], int size);
-void quickSort(int a[], int size);
 
 void print(const int a[], int iMin, int iMax);
 
